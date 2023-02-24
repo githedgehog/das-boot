88e7993d6bc365362b334324e72b64ccc856ec55
Marcus Heese <marcus@githedgehog.com>
2023-02-23 17:39:03 -0800

    extracting filepath walkdir into dedicated package

---
+++ pkg/filepath/walkdir.go
@@ -0,0 +1,61 @@
+package filepath
+
+import (
+	"io/fs"
+	"os"
+	"path/filepath"
+)
+
+var (
+	osLstat              func(name string) (fs.FileInfo, error)                 = os.Lstat
+	filepathRel          func(basepath string, targpath string) (string, error) = filepath.Rel
+	filepathEvalSymlinks func(path string) (string, error)                      = filepath.EvalSymlinks
+)
+
+// WalkDir extends filepath.WalkDir to also follow symlinks but only until maxLevel depth
+// You can specify symlinks that you do not want to follow in the exclusions list.
+// These entries are not full paths, but only file names.
+//
+// taken and adjusted from `github.com/facebookgo/symwalk`
+func WalkDir(path string, walkFn fs.WalkDirFunc, maxLevel uint, exclusions ...string) error {
+	return walkDir(path, path, walkFn, maxLevel+1, exclusions)
+}
+
+func walkDir(filename string, linkDirname string, walkFn fs.WalkDirFunc, maxLevel uint, exclusions []string) error {
+	symWalkFunc := func(path string, info fs.DirEntry, err error) error {
+
+		if fname, err := filepathRel(filename, path); err == nil {
+			path = filepath.Join(linkDirname, fname)
+		} else {
+			return err
+		}
+		var excluded bool
+		if err == nil {
+			for _, entry := range exclusions {
+				if info.Name() == entry {
+					excluded = true
+					break
+				}
+			}
+		}
+		if err == nil && info.Type()&os.ModeSymlink == os.ModeSymlink && !excluded {
+			finalPath, err := filepathEvalSymlinks(path)
+			if err != nil {
+				return walkFn(path, info, err)
+			}
+			info, err := osLstat(finalPath)
+			if err != nil {
+				return walkFn(path, fs.FileInfoToDirEntry(info), err)
+			}
+			if info.IsDir() {
+				maxLevel := maxLevel - 1
+				if maxLevel > 0 {
+					return walkDir(finalPath, path, walkFn, maxLevel, exclusions)
+				}
+			}
+		}
+
+		return walkFn(path, info, err)
+	}
+	return filepath.WalkDir(filename, symWalkFunc)
+}
---
+++ pkg/filepath/walkdir_test.go
@@ -0,0 +1,233 @@
+package filepath
+
+import (
+	"errors"
+	"fmt"
+	"io/fs"
+	"os"
+	"path/filepath"
+	"reflect"
+	"sort"
+	"testing"
+)
+
+type symwalkFn struct {
+	paths []string
+}
+
+func (s *symwalkFn) walkDirFunc(path string, d fs.DirEntry, err error) error {
+	s.paths = append(s.paths, path)
+	return nil
+}
+
+func (s *symwalkFn) walkedPaths() []string {
+	ret := make([]string, len(s.paths))
+	copy(ret, s.paths)
+	sort.Strings(ret)
+	return ret
+}
+
+func TestWalkDir(t *testing.T) {
+
+	pwd, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+	base := filepath.Join(pwd, "testdata", "WalkDir")
+
+	errFilepathRelFailed := errors.New("filepath.Rel failed")
+	errFilepathEvalSymlinksFailed := errors.New("filepath.EvalSymlinks failed")
+
+	type args struct {
+		path       string
+		maxLevel   uint
+		exclusions []string
+	}
+	tests := []struct {
+		name                 string
+		args                 args
+		wantErr              bool
+		wantErrToBe          error
+		wantPaths            []string
+		filepathRel          func(basepath string, targpath string) (string, error)
+		filepathEvalSymlinks func(path string) (string, error)
+		osLstat              func(name string) (fs.FileInfo, error)
+	}{
+		{
+			name: "no subfolder",
+			args: args{
+				path:       filepath.Join(base, "flat"),
+				maxLevel:   1,
+				exclusions: []string{},
+			},
+			wantErr: false,
+			wantPaths: []string{
+				filepath.Join(base, "flat"),
+				filepath.Join(base, "flat", "no-subfolder"),
+			},
+		},
+		{
+			name: "no subfolder with exclusions",
+			args: args{
+				path:       filepath.Join(base, "flat2"),
+				maxLevel:   1,
+				exclusions: []string{"exclude-symlink"},
+			},
+			wantErr: false,
+			wantPaths: []string{
+				filepath.Join(base, "flat2"),
+				filepath.Join(base, "flat2", "exclude-symlink"),
+				filepath.Join(base, "flat2", "no-subfolder"),
+			},
+		},
+		{
+			name:        "filepath rel fails",
+			wantErr:     true,
+			wantErrToBe: errFilepathRelFailed,
+			filepathRel: func(basepath, targpath string) (string, error) {
+				return "", errFilepathRelFailed
+			},
+			wantPaths: []string{},
+		},
+		{
+			name: "resolving one level of symlinks",
+			args: args{
+				path:       filepath.Join(base, "has-symlinks"),
+				maxLevel:   1,
+				exclusions: []string{},
+			},
+			wantErr: false,
+			wantPaths: []string{
+				filepath.Join(base, "has-symlinks"),
+				filepath.Join(base, "has-symlinks", "flat"),
+				filepath.Join(base, "has-symlinks", "flat", "no-subfolder"),
+			},
+		},
+		{
+			name: "filepath evalsymlinks failed",
+			args: args{
+				path:       filepath.Join(base, "has-symlinks"),
+				maxLevel:   1,
+				exclusions: []string{},
+			},
+			wantErr: false,
+			filepathEvalSymlinks: func(path string) (string, error) {
+				return "", errFilepathEvalSymlinksFailed
+			},
+			wantPaths: []string{
+				filepath.Join(base, "has-symlinks"),
+				filepath.Join(base, "has-symlinks", "flat"),
+			},
+		},
+		{
+			name: "resolving two level of symlinks",
+			args: args{
+				path:       filepath.Join(base, "has-nested-symlinks"),
+				maxLevel:   2,
+				exclusions: []string{},
+			},
+			wantErr: false,
+			wantPaths: []string{
+				filepath.Join(base, "has-nested-symlinks"),
+				filepath.Join(base, "has-nested-symlinks", "has-symlinks"),
+				filepath.Join(base, "has-nested-symlinks", "has-symlinks", "flat"),
+				filepath.Join(base, "has-nested-symlinks", "has-symlinks", "flat", "no-subfolder"),
+			},
+		},
+		{
+			name: "abort level of symlinks after one level",
+			args: args{
+				path:       filepath.Join(base, "has-nested-symlinks"),
+				maxLevel:   1,
+				exclusions: []string{},
+			},
+			wantErr: false,
+			wantPaths: []string{
+				filepath.Join(base, "has-nested-symlinks"),
+				filepath.Join(base, "has-nested-symlinks", "has-symlinks"),
+				filepath.Join(base, "has-nested-symlinks", "has-symlinks", "flat"),
+			},
+		},
+		{
+			name: "abort level of symlinks after one level with os lstat failing",
+			args: args{
+				path:       filepath.Join(base, "has-nested-symlinks"),
+				maxLevel:   1,
+				exclusions: []string{},
+			},
+			wantErr: false,
+			osLstat: func(name string) (fs.FileInfo, error) {
+				return nil, fmt.Errorf("os lstat failed")
+			},
+			wantPaths: []string{
+				filepath.Join(base, "has-nested-symlinks"),
+				filepath.Join(base, "has-nested-symlinks", "has-symlinks"),
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if tt.filepathRel != nil {
+				oldFilepathRel := filepathRel
+				defer func() {
+					filepathRel = oldFilepathRel
+				}()
+				filepathRel = tt.filepathRel
+			}
+			if tt.filepathEvalSymlinks != nil {
+				oldFilepathEvalSymlinks := filepathEvalSymlinks
+				defer func() {
+					filepathEvalSymlinks = oldFilepathEvalSymlinks
+				}()
+				filepathEvalSymlinks = tt.filepathEvalSymlinks
+			}
+			if tt.osLstat != nil {
+				oldOsLstat := osLstat
+				defer func() {
+					osLstat = oldOsLstat
+				}()
+				osLstat = tt.osLstat
+			}
+			s := &symwalkFn{}
+			if err := WalkDir(tt.args.path, s.walkDirFunc, tt.args.maxLevel, tt.args.exclusions...); (err != nil) != tt.wantErr {
+				t.Errorf("WalkDir() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if err != nil && tt.wantErr && tt.wantErrToBe != nil {
+				if !errors.Is(err, tt.wantErrToBe) {
+					t.Errorf("WalkDir() error = %v, wantErrToBe %v", err, tt.wantErrToBe)
+					return
+				}
+			}
+			walkedPaths := s.walkedPaths()
+			if !reflect.DeepEqual(walkedPaths, tt.wantPaths) {
+				t.Errorf("WalkDir() paths = %#v, wantPaths = %#v", walkedPaths, tt.wantPaths)
+				return
+			}
+		})
+	}
+}
+
+func Test_walkDir(t *testing.T) {
+	type args struct {
+		filename    string
+		linkDirname string
+		walkFn      fs.WalkDirFunc
+		maxLevel    uint
+		exclusions  []string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := walkDir(tt.args.filename, tt.args.linkDirname, tt.args.walkFn, tt.args.maxLevel, tt.args.exclusions); (err != nil) != tt.wantErr {
+				t.Errorf("walkDir() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
