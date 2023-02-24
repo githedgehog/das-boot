89c7b07c44f6e26f62113519f7d781b1858ca33e
Marcus Heese <marcus@githedgehog.com>
2023-02-24 13:23:54 -0800

    adding hhdevid as CLI tool

---
+++ cmd/hhdevid/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"os"
+
+	"go.githedgehog.com/dasboot/pkg/devid"
+	"go.githedgehog.com/dasboot/pkg/log"
+	"go.githedgehog.com/dasboot/pkg/version"
+
+	"github.com/urfave/cli/v2"
+	"go.uber.org/zap"
+)
+
+var l = log.L()
+
+var description = `
+hhdevid determines a unique device ID for the device that it is running on
+in the form of a UUID. This ID is used throughout Hedgehog SONiC for
+identifying a running device - particularly at installation time by DAS BOOT.
+
+The hhdevid tool is meant to be running on ONIE with root privileges.
+Otherwise it could fail some of the detection mechanisms like missing
+executables like 'onie-sysinfo' or 'onie-syseeprom', or it cannot open
+/sys/class/dmi/id/product_uuid. Note that this could be expected depending
+on the device where it is running on which is why this tool is not failing in
+these cases.
+
+It tries to determine a unique ID through the following methods - falling back
+to the next if the previous one failed:
+
+1. Using ONIE sysinfo and EEPROM information: vendor ID + serial number
+2. on x86: through the System UUID of the DMI
+3. on arm: through the serial number of the CPU
+4. through all MAC addresses of all physical network devices
+
+To use the device ID in a script, pay attention to redirect stderr
+to /dev/null to avoid capturing potential log messages:
+
+hhdevid 2>/dev/null
+`
+
+func main() {
+	app := &cli.App{
+		Name:        "hhdevid",
+		Usage:       "device identification tool",
+		UsageText:   "hhdevid",
+		Description: description[1 : len(description)-1],
+		Version:     version.Version,
+		Action: func(ctx *cli.Context) error {
+			devid := devid.ID()
+			_, err := os.Stdout.WriteString(devid + "\n")
+			return err
+		},
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		l.Fatal("integ-disk failed", zap.Error(err))
+	}
+}
