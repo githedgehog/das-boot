f37c4d0d3c942bf5c9f302e453380cc6c80972b4
Marcus Heese <marcus@githedgehog.com>
2023-03-09 16:32:43 -0800

    ported zap syslog package which was unmaintained

---
+++ pkg/log/syslog/encoder.go
@@ -0,0 +1,293 @@
+// Copyright (c) 2017 Timon Wong
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package syslog
+
+import (
+	"os"
+	"path"
+	"strings"
+	"time"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/buffer"
+	"go.uber.org/zap/zapcore"
+)
+
+const (
+	version         = 1
+	severityMask    = 0x07
+	facilityMask    = 0xf8
+	nilValue        = "-"
+	timestampFormat = "2006-01-02T15:04:05.000000Z07:00" // RFC3339 with micro fraction seconds
+	maxHostnameLen  = 255
+	maxAppNameLen   = 48
+)
+
+var bufferpool = buffer.NewPool()
+
+var (
+	_ zapcore.Encoder = &syslogEncoder{}
+	_                 = zapcore.NewJSONEncoder(zap.NewProductionEncoderConfig()).(jsonEncoder)
+)
+
+// Framing.
+const (
+	NonTransparentFraming Framing = iota
+	OctetCountingFraming
+	DefaultFraming = NonTransparentFraming
+)
+
+// Framing configures RFC6587 TCP transport framing.
+type Framing int
+
+type jsonEncoder interface {
+	zapcore.Encoder
+	zapcore.ArrayEncoder
+}
+
+// SyslogEncoderConfig allows users to configure the concrete encoders for zap syslog.
+type SyslogEncoderConfig struct {
+	zapcore.EncoderConfig
+
+	Framing  Framing  `json:"framing" yaml:"framing"`
+	Facility Priority `json:"facility" yaml:"facility"`
+	Hostname string   `json:"hostname" yaml:"hostname"`
+	PID      int      `json:"pid" yaml:"pid"`
+	App      string   `json:"app" yaml:"app"`
+}
+
+type syslogEncoder struct {
+	*SyslogEncoderConfig
+	je jsonEncoder
+}
+
+func rfc5424CompliantASCIIMapper(r rune) rune {
+	// PRINTUSASCII    = %d33-126
+	if r < 33 || r > 126 {
+		return '_'
+	}
+	return r
+}
+
+func toRFC5424CompliantASCIIString(s string) string {
+	return strings.Map(rfc5424CompliantASCIIMapper, s)
+}
+
+// NewSyslogEncoder creates a syslogEncoder.
+func NewSyslogEncoder(cfg SyslogEncoderConfig) zapcore.Encoder {
+	if cfg.Hostname == "" {
+		hostname, _ := os.Hostname()
+		cfg.Hostname = hostname
+	}
+	if cfg.Hostname == "" {
+		cfg.Hostname = nilValue
+	} else {
+		hostname := toRFC5424CompliantASCIIString(cfg.Hostname)
+		if len(hostname) > maxHostnameLen {
+			hostname = hostname[:maxHostnameLen]
+		}
+		cfg.Hostname = hostname
+	}
+
+	if cfg.PID == 0 {
+		cfg.PID = os.Getpid()
+	}
+	if cfg.App == "" {
+		cfg.App = nilValue
+	} else {
+		app := cfg.App
+		if len(app) > maxAppNameLen {
+			app = path.Base(app)
+		}
+		if len(app) > maxAppNameLen {
+			app = app[:maxAppNameLen]
+		}
+		app = toRFC5424CompliantASCIIString(app)
+		cfg.App = app
+	}
+
+	cfg.EncoderConfig.LineEnding = "\n"
+	je := zapcore.NewJSONEncoder(cfg.EncoderConfig).(jsonEncoder)
+	return &syslogEncoder{
+		SyslogEncoderConfig: &cfg,
+		je:                  je,
+	}
+}
+
+func (enc *syslogEncoder) AddArray(key string, arr zapcore.ArrayMarshaler) error {
+	return enc.je.AddArray(key, arr)
+}
+
+func (enc *syslogEncoder) AddObject(key string, obj zapcore.ObjectMarshaler) error {
+	return enc.je.AddObject(key, obj)
+}
+
+func (enc *syslogEncoder) AddBinary(key string, val []byte)          { enc.je.AddBinary(key, val) }
+func (enc *syslogEncoder) AddByteString(key string, val []byte)      { enc.je.AddByteString(key, val) }
+func (enc *syslogEncoder) AddBool(key string, val bool)              { enc.je.AddBool(key, val) }
+func (enc *syslogEncoder) AddComplex128(key string, val complex128)  { enc.je.AddComplex128(key, val) }
+func (enc *syslogEncoder) AddDuration(key string, val time.Duration) { enc.je.AddDuration(key, val) }
+func (enc *syslogEncoder) AddFloat64(key string, val float64)        { enc.je.AddFloat64(key, val) }
+func (enc *syslogEncoder) AddInt64(key string, val int64)            { enc.je.AddInt64(key, val) }
+
+func (enc *syslogEncoder) AddReflected(key string, obj interface{}) error {
+	return enc.je.AddReflected(key, obj)
+}
+
+func (enc *syslogEncoder) OpenNamespace(key string)          { enc.je.OpenNamespace(key) }
+func (enc *syslogEncoder) AddString(key, val string)         { enc.je.AddString(key, val) }
+func (enc *syslogEncoder) AddTime(key string, val time.Time) { enc.je.AddTime(key, val) }
+func (enc *syslogEncoder) AddUint64(key string, val uint64)  { enc.je.AddUint64(key, val) }
+
+func (enc *syslogEncoder) AppendArray(arr zapcore.ArrayMarshaler) error {
+	return enc.je.AppendArray(arr)
+}
+
+func (enc *syslogEncoder) AppendObject(obj zapcore.ObjectMarshaler) error {
+	return enc.je.AppendObject(obj)
+}
+
+func (enc *syslogEncoder) AppendBool(val bool)              { enc.je.AppendBool(val) }
+func (enc *syslogEncoder) AppendByteString(val []byte)      { enc.je.AppendByteString(val) }
+func (enc *syslogEncoder) AppendComplex128(val complex128)  { enc.je.AppendComplex128(val) }
+func (enc *syslogEncoder) AppendDuration(val time.Duration) { enc.je.AppendDuration(val) }
+func (enc *syslogEncoder) AppendInt64(val int64)            { enc.je.AppendInt64(val) }
+
+func (enc *syslogEncoder) AppendReflected(val interface{}) error {
+	return enc.je.AppendReflected(val)
+}
+
+func (enc *syslogEncoder) AppendString(val string)            { enc.je.AppendString(val) }
+func (enc *syslogEncoder) AppendTime(val time.Time)           { enc.je.AppendTime(val) }
+func (enc *syslogEncoder) AppendUint64(val uint64)            { enc.je.AppendUint64(val) }
+func (enc *syslogEncoder) AddComplex64(k string, v complex64) { enc.je.AddComplex64(k, v) }
+func (enc *syslogEncoder) AddFloat32(k string, v float32)     { enc.je.AddFloat32(k, v) }
+func (enc *syslogEncoder) AddInt(k string, v int)             { enc.je.AddInt(k, v) }
+func (enc *syslogEncoder) AddInt32(k string, v int32)         { enc.je.AddInt32(k, v) }
+func (enc *syslogEncoder) AddInt16(k string, v int16)         { enc.je.AddInt16(k, v) }
+func (enc *syslogEncoder) AddInt8(k string, v int8)           { enc.je.AddInt8(k, v) }
+func (enc *syslogEncoder) AddUint(k string, v uint)           { enc.je.AddUint(k, v) }
+func (enc *syslogEncoder) AddUint32(k string, v uint32)       { enc.je.AddUint32(k, v) }
+func (enc *syslogEncoder) AddUint16(k string, v uint16)       { enc.je.AddUint16(k, v) }
+func (enc *syslogEncoder) AddUint8(k string, v uint8)         { enc.je.AddUint8(k, v) }
+func (enc *syslogEncoder) AddUintptr(k string, v uintptr)     { enc.je.AddUintptr(k, v) }
+func (enc *syslogEncoder) AppendComplex64(v complex64)        { enc.je.AppendComplex64(v) }
+func (enc *syslogEncoder) AppendFloat64(v float64)            { enc.je.AppendFloat64(v) }
+func (enc *syslogEncoder) AppendFloat32(v float32)            { enc.je.AppendFloat32(v) }
+func (enc *syslogEncoder) AppendInt(v int)                    { enc.je.AppendInt(v) }
+func (enc *syslogEncoder) AppendInt32(v int32)                { enc.je.AppendInt32(v) }
+func (enc *syslogEncoder) AppendInt16(v int16)                { enc.je.AppendInt16(v) }
+func (enc *syslogEncoder) AppendInt8(v int8)                  { enc.je.AppendInt8(v) }
+func (enc *syslogEncoder) AppendUint(v uint)                  { enc.je.AppendUint(v) }
+func (enc *syslogEncoder) AppendUint32(v uint32)              { enc.je.AppendUint32(v) }
+func (enc *syslogEncoder) AppendUint16(v uint16)              { enc.je.AppendUint16(v) }
+func (enc *syslogEncoder) AppendUint8(v uint8)                { enc.je.AppendUint8(v) }
+func (enc *syslogEncoder) AppendUintptr(v uintptr)            { enc.je.AppendUintptr(v) }
+
+func (enc *syslogEncoder) Clone() zapcore.Encoder {
+	return enc.clone()
+}
+
+func (enc *syslogEncoder) clone() *syslogEncoder {
+	clone := &syslogEncoder{
+		SyslogEncoderConfig: enc.SyslogEncoderConfig,
+		je:                  enc.je.Clone().(jsonEncoder),
+	}
+	return clone
+}
+
+func (enc *syslogEncoder) EncodeEntry(ent zapcore.Entry, fields []zapcore.Field) (*buffer.Buffer, error) {
+	msg := bufferpool.Get()
+
+	var p Priority
+	switch ent.Level {
+	case zapcore.InvalidLevel:
+		// ignore
+	case zapcore.FatalLevel:
+		p = LOG_EMERG
+	case zapcore.PanicLevel:
+		p = LOG_CRIT
+	case zapcore.DPanicLevel:
+		p = LOG_CRIT
+	case zapcore.ErrorLevel:
+		p = LOG_ERR
+	case zapcore.WarnLevel:
+		p = LOG_WARNING
+	case zapcore.InfoLevel:
+		p = LOG_INFO
+	case zapcore.DebugLevel:
+		p = LOG_DEBUG
+	}
+	pr := int64((enc.Facility & facilityMask) | (p & severityMask))
+
+	// <PRI>version
+	msg.AppendByte('<')
+	msg.AppendInt(pr)
+	msg.AppendByte('>')
+	msg.AppendInt(version)
+
+	// SP TIMESTAMP
+	msg.AppendByte(' ')
+	if ent.Time.IsZero() {
+		msg.AppendString(nilValue)
+	} else {
+		msg.AppendString(ent.Time.Format(timestampFormat))
+	}
+
+	// SP HOSTNAME
+	msg.AppendByte(' ')
+	msg.AppendString(enc.Hostname)
+
+	// SP APP-NAME
+	msg.AppendByte(' ')
+	msg.AppendString(enc.App)
+
+	// SP PROCID
+	msg.AppendByte(' ')
+	msg.AppendInt(int64(enc.PID))
+
+	// SP MSGID SP STRUCTURED-DATA (just ignore)
+	msg.AppendString(" - -")
+
+	// SP UTF8 MSG
+	json, err := enc.je.EncodeEntry(ent, fields)
+	if json.Len() > 0 {
+		msg.AppendString(" \xef\xbb\xbf")
+		bs := json.Bytes()
+		if enc.Framing == OctetCountingFraming {
+			// Strip trailing line feed
+			bs = bs[:len(bs)-1]
+		}
+		msg.AppendString(string(bs))
+	}
+
+	if enc.Framing != OctetCountingFraming {
+		return msg, err
+	}
+
+	// SYSLOG-FRAME = MSG-LEN SP SYSLOG-MSG
+	out := bufferpool.Get()
+	out.AppendInt(int64(msg.Len()))
+	out.AppendByte(' ')
+	out.AppendString(string(msg.Bytes()))
+	msg.Free()
+	return out, err
+}
---
+++ pkg/log/syslog/encoder_test.go
@@ -0,0 +1,495 @@
+// Modifications Copyright (c) 2017 Timon Wong
+// Copyright (c) 2016 Uber Technologies, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package syslog
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"math"
+	"reflect"
+	"strings"
+	"testing"
+	"time"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+var (
+	testEntry = zapcore.Entry{
+		Time:    time.Date(2017, 1, 2, 3, 4, 5, 123456789, time.UTC),
+		Message: "fake",
+		Level:   zap.DebugLevel,
+	}
+)
+
+func TestToRFC5424CompliantASCIIString(t *testing.T) {
+	fixtures := []struct {
+		s        string
+		expected string
+	}{
+		{
+			s:        " abc ",
+			expected: "_abc_",
+		},
+		{
+			s:        "中文",
+			expected: "__",
+		},
+		{
+			s:        "\x00\x01\x02\x03\x04test",
+			expected: "_____test",
+		},
+	}
+
+	for _, f := range fixtures {
+		actual := toRFC5424CompliantASCIIString(f.s)
+		if !reflect.DeepEqual(actual, f.expected) {
+			t.Errorf("toRFC5424CompliantASCIIString() got = %#v, want = %#v", actual, f.expected)
+			return
+		}
+	}
+}
+
+func TestJSONEncoderObjectFields(t *testing.T) {
+	tests := []struct {
+		desc     string
+		expected string
+		f        func(zapcore.Encoder)
+	}{
+		{"binary", `"k":"YWIxMg=="`, func(e zapcore.Encoder) { e.AddBinary("k", []byte("ab12")) }},
+		{"bool", `"k\\":true`, func(e zapcore.Encoder) { e.AddBool(`k\`, true) }}, // test key escaping once
+		{"bool", `"k":true`, func(e zapcore.Encoder) { e.AddBool("k", true) }},
+		{"bool", `"k":false`, func(e zapcore.Encoder) { e.AddBool("k", false) }},
+		{"byteString", `"k":"v\\"`, func(e zapcore.Encoder) { e.AddByteString(`k`, []byte(`v\`)) }},
+		{"byteString", `"k":"v"`, func(e zapcore.Encoder) { e.AddByteString("k", []byte("v")) }},
+		{"byteString", `"k":""`, func(e zapcore.Encoder) { e.AddByteString("k", []byte{}) }},
+		{"byteString", `"k":""`, func(e zapcore.Encoder) { e.AddByteString("k", nil) }},
+		{"complex128", `"k":"1+2i"`, func(e zapcore.Encoder) { e.AddComplex128("k", 1+2i) }},
+		{"complex64", `"k":"1+2i"`, func(e zapcore.Encoder) { e.AddComplex64("k", 1+2i) }},
+		{"duration", `"k":0.000000001`, func(e zapcore.Encoder) { e.AddDuration("k", 1) }},
+		{"float64", `"k":1`, func(e zapcore.Encoder) { e.AddFloat64("k", 1.0) }},
+		{"float64", `"k":10000000000`, func(e zapcore.Encoder) { e.AddFloat64("k", 1e10) }},
+		{"float64", `"k":"NaN"`, func(e zapcore.Encoder) { e.AddFloat64("k", math.NaN()) }},
+		{"float64", `"k":"+Inf"`, func(e zapcore.Encoder) { e.AddFloat64("k", math.Inf(1)) }},
+		{"float64", `"k":"-Inf"`, func(e zapcore.Encoder) { e.AddFloat64("k", math.Inf(-1)) }},
+		{"float32", `"k":1`, func(e zapcore.Encoder) { e.AddFloat32("k", 1.0) }},
+		{"float32", `"k":10000000000`, func(e zapcore.Encoder) { e.AddFloat32("k", 1e10) }},
+		{"float32", `"k":"NaN"`, func(e zapcore.Encoder) { e.AddFloat32("k", float32(math.NaN())) }},
+		{"float32", `"k":"+Inf"`, func(e zapcore.Encoder) { e.AddFloat32("k", float32(math.Inf(1))) }},
+		{"float32", `"k":"-Inf"`, func(e zapcore.Encoder) { e.AddFloat32("k", float32(math.Inf(-1))) }},
+		{"int", `"k":42`, func(e zapcore.Encoder) { e.AddInt("k", 42) }},
+		{"int64", `"k":42`, func(e zapcore.Encoder) { e.AddInt64("k", 42) }},
+		{"int32", `"k":42`, func(e zapcore.Encoder) { e.AddInt32("k", 42) }},
+		{"int16", `"k":42`, func(e zapcore.Encoder) { e.AddInt16("k", 42) }},
+		{"int8", `"k":42`, func(e zapcore.Encoder) { e.AddInt8("k", 42) }},
+		{"string", `"k":"v\\"`, func(e zapcore.Encoder) { e.AddString(`k`, `v\`) }},
+		{"string", `"k":"v"`, func(e zapcore.Encoder) { e.AddString("k", "v") }},
+		{"string", `"k":""`, func(e zapcore.Encoder) { e.AddString("k", "") }},
+		{"time", `"k":1`, func(e zapcore.Encoder) { e.AddTime("k", time.Unix(1, 0)) }},
+		{"uint", `"k":42`, func(e zapcore.Encoder) { e.AddUint("k", 42) }},
+		{"uint64", `"k":42`, func(e zapcore.Encoder) { e.AddUint64("k", 42) }},
+		{"uint32", `"k":42`, func(e zapcore.Encoder) { e.AddUint32("k", 42) }},
+		{"uint16", `"k":42`, func(e zapcore.Encoder) { e.AddUint16("k", 42) }},
+		{"uint8", `"k":42`, func(e zapcore.Encoder) { e.AddUint8("k", 42) }},
+		{"uintptr", `"k":42`, func(e zapcore.Encoder) { e.AddUintptr("k", 42) }},
+		{
+			desc:     "object (success)",
+			expected: `"k":{"loggable":"yes"}`,
+			f: func(e zapcore.Encoder) {
+				err := e.AddObject("k", loggable{true})
+				if err != nil {
+					t.Errorf("Unexpected error calling MarshalLogObject: %v", err)
+					return
+				}
+			},
+		},
+		{
+			desc:     "object (error)",
+			expected: `"k":{}`,
+			f: func(e zapcore.Encoder) {
+				err := e.AddObject("k", loggable{false})
+				if err == nil {
+					t.Errorf("Unexpected error calling MarshalLogObject")
+					return
+				}
+			},
+		},
+		{
+			desc:     "object (with nested array)",
+			expected: `"turducken":{"ducks":[{"in":"chicken"},{"in":"chicken"}]}`,
+			f: func(e zapcore.Encoder) {
+				err := e.AddObject("turducken", turducken{})
+				if err != nil {
+					t.Errorf("Unexpected error calling MarshalLogObject with nested ObjectMarshalers and ArrayMarshalers: %v", err)
+					return
+				}
+			},
+		},
+		{
+			desc:     "array (with nested object)",
+			expected: `"turduckens":[{"ducks":[{"in":"chicken"},{"in":"chicken"}]},{"ducks":[{"in":"chicken"},{"in":"chicken"}]}]`,
+			f: func(e zapcore.Encoder) {
+				err := e.AddArray("turduckens", turduckens(2))
+				if err != nil {
+					t.Errorf("Unexpected error calling MarshalLogObject with nested ObjectMarshalers and ArrayMarshalers: %v", err)
+					return
+				}
+			},
+		},
+		{
+			desc:     "array (success)",
+			expected: `"k":[true]`,
+			f: func(e zapcore.Encoder) {
+				err := e.AddArray(`k`, loggable{true})
+				if err != nil {
+					t.Errorf("Unexpected error calling MarshalLogArray: %v", err)
+					return
+				}
+			},
+		},
+		{
+			desc:     "array (error)",
+			expected: `"k":[]`,
+			f: func(e zapcore.Encoder) {
+				err := e.AddArray("k", loggable{false})
+				if err == nil {
+					t.Errorf("Unexpected error calling MarshalLogArray")
+					return
+				}
+			},
+		},
+		{
+			desc:     "reflect (success)",
+			expected: `"k":{"loggable":"yes"}`,
+			f: func(e zapcore.Encoder) {
+				err := e.AddReflected("k", map[string]string{"loggable": "yes"})
+				if err != nil {
+					t.Errorf("Unexpected error JSON-serializing a map: %v", err)
+					return
+				}
+			},
+		},
+		{
+			desc:     "reflect (failure)",
+			expected: "",
+			f: func(e zapcore.Encoder) {
+				err := e.AddReflected("k", noJSON{})
+				if err == nil {
+					t.Errorf("Unexpected success JSON-serializing a noJSON")
+					return
+				}
+			},
+		},
+		{
+			desc: "namespace",
+			// EncodeEntry is responsible for closing all open namespaces.
+			expected: `"outermost":{"outer":{"foo":1,"inner":{"foo":2,"innermost":{`,
+			f: func(e zapcore.Encoder) {
+				e.OpenNamespace("outermost")
+				e.OpenNamespace("outer")
+				e.AddInt("foo", 1)
+				e.OpenNamespace("inner")
+				e.AddInt("foo", 2)
+				e.OpenNamespace("innermost")
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		assertOutput(t, tt.desc, tt.expected, tt.f)
+	}
+}
+
+func TestJSONEncoderArrays(t *testing.T) {
+	tests := []struct {
+		desc     string
+		expected string // expect f to be called twice
+		f        func(zapcore.ArrayEncoder)
+	}{
+		{"bool", `[true,true]`, func(e zapcore.ArrayEncoder) { e.AppendBool(true) }},
+		{"byteString", `["k","k"]`, func(e zapcore.ArrayEncoder) { e.AppendByteString([]byte("k")) }},
+		{"byteString", `["k\\","k\\"]`, func(e zapcore.ArrayEncoder) { e.AppendByteString([]byte(`k\`)) }},
+		{"complex128", `["1+2i","1+2i"]`, func(e zapcore.ArrayEncoder) { e.AppendComplex128(1 + 2i) }},
+		{"complex64", `["1+2i","1+2i"]`, func(e zapcore.ArrayEncoder) { e.AppendComplex64(1 + 2i) }},
+		{"durations", `[0.000000002,0.000000002]`, func(e zapcore.ArrayEncoder) { e.AppendDuration(2) }},
+		{"float64", `[3.14,3.14]`, func(e zapcore.ArrayEncoder) { e.AppendFloat64(3.14) }},
+		{"float32", `[3.14,3.14]`, func(e zapcore.ArrayEncoder) { e.AppendFloat32(3.14) }},
+		{"int", `[42,42]`, func(e zapcore.ArrayEncoder) { e.AppendInt(42) }},
+		{"int64", `[42,42]`, func(e zapcore.ArrayEncoder) { e.AppendInt64(42) }},
+		{"int32", `[42,42]`, func(e zapcore.ArrayEncoder) { e.AppendInt32(42) }},
+		{"int16", `[42,42]`, func(e zapcore.ArrayEncoder) { e.AppendInt16(42) }},
+		{"int8", `[42,42]`, func(e zapcore.ArrayEncoder) { e.AppendInt8(42) }},
+		{"string", `["k","k"]`, func(e zapcore.ArrayEncoder) { e.AppendString("k") }},
+		{"string", `["k\\","k\\"]`, func(e zapcore.ArrayEncoder) { e.AppendString(`k\`) }},
+		{"times", `[1,1]`, func(e zapcore.ArrayEncoder) { e.AppendTime(time.Unix(1, 0)) }},
+		{"uint", `[42,42]`, func(e zapcore.ArrayEncoder) { e.AppendUint(42) }},
+		{"uint64", `[42,42]`, func(e zapcore.ArrayEncoder) { e.AppendUint64(42) }},
+		{"uint32", `[42,42]`, func(e zapcore.ArrayEncoder) { e.AppendUint32(42) }},
+		{"uint16", `[42,42]`, func(e zapcore.ArrayEncoder) { e.AppendUint16(42) }},
+		{"uint8", `[42,42]`, func(e zapcore.ArrayEncoder) { e.AppendUint8(42) }},
+		{"uintptr", `[42,42]`, func(e zapcore.ArrayEncoder) { e.AppendUintptr(42) }},
+		{
+			desc:     "arrays (success)",
+			expected: `[[true],[true]]`,
+			f: func(arr zapcore.ArrayEncoder) {
+				err := arr.AppendArray(zapcore.ArrayMarshalerFunc(func(inner zapcore.ArrayEncoder) error {
+					inner.AppendBool(true)
+					return nil
+				}))
+				if err != nil {
+					t.Errorf("Unexpected error appending an array: %v", err)
+					return
+				}
+			},
+		},
+		{
+			desc:     "arrays (error)",
+			expected: `[[true],[true]]`,
+			f: func(arr zapcore.ArrayEncoder) {
+				err := arr.AppendArray(zapcore.ArrayMarshalerFunc(func(inner zapcore.ArrayEncoder) error {
+					inner.AppendBool(true)
+					return errors.New("fail")
+				}))
+				if err == nil {
+					t.Errorf("Expected an error appending an array")
+					return
+				}
+			},
+		},
+		{
+			desc:     "objects (success)",
+			expected: `[{"loggable":"yes"},{"loggable":"yes"}]`,
+			f: func(arr zapcore.ArrayEncoder) {
+				err := arr.AppendObject(loggable{true})
+				if err != nil {
+					t.Errorf("Unexpected error appending an object: %v", err)
+				}
+			},
+		},
+		{
+			desc:     "objects (error)",
+			expected: `[{},{}]`,
+			f: func(arr zapcore.ArrayEncoder) {
+				err := arr.AppendObject(loggable{false})
+				if err == nil {
+					t.Errorf("Expected an error appending an object")
+					return
+				}
+			},
+		},
+		{
+			desc:     "reflect (success)",
+			expected: `[{"foo":5},{"foo":5}]`,
+			f: func(arr zapcore.ArrayEncoder) {
+				err := arr.AppendReflected(map[string]int{"foo": 5})
+				if err != nil {
+					t.Errorf("Unexpected an error appending an object with reflection: %v", err)
+					return
+				}
+			},
+		},
+		{
+			desc:     "reflect (error)",
+			expected: `[]`,
+			f: func(arr zapcore.ArrayEncoder) {
+				err := arr.AppendReflected(noJSON{})
+				if err == nil {
+					t.Errorf("Unexpected an error appending an object with reflection")
+					return
+				}
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		f := func(enc zapcore.Encoder) error {
+			return enc.AddArray("array", zapcore.ArrayMarshalerFunc(func(arr zapcore.ArrayEncoder) error {
+				tt.f(arr)
+				tt.f(arr)
+				return nil
+			}))
+		}
+		assertOutput(t, tt.desc, `"array":`+tt.expected, func(enc zapcore.Encoder) {
+			err := f(enc)
+			if err != nil {
+				t.Errorf("Unexpected error adding array to JSON zapcore.Encoder: %v", err)
+				return
+			}
+		})
+	}
+}
+
+func assertOutput(t testing.TB, _ string, expected string, f func(zapcore.Encoder)) {
+	enc := NewSyslogEncoder(SyslogEncoderConfig{
+		EncoderConfig: zapcore.EncoderConfig{
+			EncodeTime:     zapcore.EpochTimeEncoder,
+			EncodeDuration: zapcore.SecondsDurationEncoder,
+		},
+	}).(*syslogEncoder)
+	f(enc)
+	buf, err := enc.EncodeEntry(testEntry, nil)
+	if err != nil {
+		t.Errorf("unexpected error in call to EncodeEntry: %v", err)
+		return
+	}
+	defer buf.Free()
+
+	const bom = "\xef\xbb\xbf"
+	output := buf.String()
+	i := strings.Index(output, "\xef\xbb\xbf")
+	// TODO
+	// require.Condition(t, func() (success bool) { return i > 0 }, "not a valid syslog output")
+	if i <= 0 {
+		t.Errorf("not a valid syslog output")
+		return
+	}
+
+	jsonString := output[i+len(bom):]
+	if !strings.Contains(jsonString, expected) {
+		t.Errorf("JSON '%v' does not contain '%v'", jsonString, expected)
+		return
+	}
+}
+
+// Nested Array- and ObjectMarshalers.
+type turducken struct{}
+
+func (t turducken) MarshalLogObject(enc zapcore.ObjectEncoder) error {
+	return enc.AddArray("ducks", zapcore.ArrayMarshalerFunc(func(arr zapcore.ArrayEncoder) error {
+		for i := 0; i < 2; i++ {
+			arr.AppendObject(zapcore.ObjectMarshalerFunc(func(inner zapcore.ObjectEncoder) error { //nolint:errcheck
+				inner.AddString("in", "chicken")
+				return nil
+			}))
+		}
+		return nil
+	}))
+}
+
+type turduckens int
+
+func (t turduckens) MarshalLogArray(enc zapcore.ArrayEncoder) error {
+	var err error
+	tur := turducken{}
+	for i := 0; i < int(t); i++ {
+		// TODO
+		// err = multierr.Append(err, enc.AppendObject(tur))
+		err = enc.AppendObject(tur)
+	}
+	return err
+}
+
+type loggable struct{ bool }
+
+func (l loggable) MarshalLogObject(enc zapcore.ObjectEncoder) error {
+	if !l.bool {
+		return errors.New("can't marshal")
+	}
+	enc.AddString("loggable", "yes")
+	return nil
+}
+
+func (l loggable) MarshalLogArray(enc zapcore.ArrayEncoder) error {
+	if !l.bool {
+		return errors.New("can't marshal")
+	}
+	enc.AppendBool(true)
+	return nil
+}
+
+type noJSON struct{}
+
+func (nj noJSON) MarshalJSON() ([]byte, error) {
+	return nil, errors.New("no")
+}
+
+func testEncoderConfig(framing Framing) SyslogEncoderConfig {
+	return SyslogEncoderConfig{
+		EncoderConfig: zapcore.EncoderConfig{
+			MessageKey:     "msg",
+			NameKey:        "name",
+			CallerKey:      "caller",
+			StacktraceKey:  "stacktrace",
+			EncodeTime:     zapcore.EpochTimeEncoder,
+			EncodeLevel:    zapcore.LowercaseLevelEncoder,
+			EncodeDuration: zapcore.SecondsDurationEncoder,
+			EncodeCaller:   zapcore.ShortCallerEncoder,
+		},
+
+		Framing:  framing,
+		Hostname: "localhost",
+		App:      "encoder_test",
+		PID:      9876,
+		Facility: LOG_LOCAL0,
+	}
+}
+
+func testSyslogEncoderFraming(t *testing.T, framing Framing) {
+	enc := NewSyslogEncoder(testEncoderConfig(framing))
+	enc.AddString("str", "foo")
+	enc.AddInt64("int64-1", 1)
+	enc.AddInt64("int64-2", 2)
+	enc.AddFloat64("float64", 1.0)
+	enc.AddString("string1", "\n")
+	enc.AddString("string2", "💩")
+	enc.AddString("string3", "🤔")
+	enc.AddString("string4", "🙊")
+	enc.AddBool("bool", true)
+	buf, _ := enc.EncodeEntry(testEntry, nil)
+	defer buf.Free()
+
+	msg := buf.String()
+	msgPrefix := "<135>1 2017-01-02T03:04:05.123456Z localhost encoder_test 9876 - - \xef\xbb\xbf"
+	if framing == OctetCountingFraming {
+		spacePos := strings.Index(msg, " ") + 1
+		msgPrefix = fmt.Sprintf("%d %s", buf.Len()-spacePos, msgPrefix)
+	}
+
+	if !strings.HasPrefix(msg, msgPrefix) {
+		t.Errorf("Wrong syslog output for framing: %d", framing)
+		t.Log(msg)
+		t.Log(msgPrefix)
+		return
+	}
+
+	if framing == OctetCountingFraming && strings.HasSuffix(msg, "\n") {
+		t.Errorf("syslog output for OctetCountingFraming should not ends with `\\n`")
+		return
+	}
+
+	jsonString := msg[len(msgPrefix):]
+	var m map[string]interface{}
+	err := json.Unmarshal([]byte(jsonString), &m)
+	if err != nil {
+		t.Errorf("message part of syslog output is not a valid json string: %s", err)
+		t.Logf("actual got: %s", jsonString)
+		return
+	}
+}
+
+func TestSyslogEncoder(t *testing.T) {
+	for _, framing := range []Framing{NonTransparentFraming, OctetCountingFraming} {
+		testSyslogEncoderFraming(t, framing)
+	}
+}
