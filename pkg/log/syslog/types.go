33874642f36992f68918006bda1197a00468a73e
Marcus Heese <marcus@githedgehog.com>
2023-03-14 00:32:57 -0700

    made syslog facilities cli flag compatible

--- pkg/log/syslog/types.go
+++ pkg/log/syslog/types.go
@@ -90,3 +90,81 @@ func FacilityPriority(facility string) (Priority, error) {
 	}
 	return 0, fmt.Errorf("invalid syslog facility: %s", facility)
 }
+
+func (l Priority) String() string {
+	switch l {
+	// priorities (NOTE: EMERG missing because of overlapping KERN)
+	case LOG_ALERT:
+		return "ALERT"
+	case LOG_CRIT:
+		return "CRIT"
+	case LOG_ERR:
+		return "ERR"
+	case LOG_WARNING:
+		return "WARNING"
+	case LOG_NOTICE:
+		return "NOTICE"
+	case LOG_INFO:
+		return "INFO"
+	case LOG_DEBUG:
+		return "DEBUG"
+	// facilities
+	case LOG_KERN:
+		return "KERN"
+	case LOG_USER:
+		return "USER"
+	case LOG_MAIL:
+		return "MAIL"
+	case LOG_DAEMON:
+		return "DAEMON"
+	case LOG_AUTH:
+		return "AUTH"
+	case LOG_SYSLOG:
+		return "SYSLOG"
+	case LOG_LPR:
+		return "LPR"
+	case LOG_NEWS:
+		return "NEWS"
+	case LOG_UUCP:
+		return "UUCP"
+	case LOG_CRON:
+		return "CRON"
+	case LOG_AUTHPRIV:
+		return "AUTHPRIV"
+	case LOG_FTP:
+		return "FTP"
+	case LOG_LOCAL0:
+		return "LOCAL0"
+	case LOG_LOCAL1:
+		return "LOCAL1"
+	case LOG_LOCAL2:
+		return "LOCAL2"
+	case LOG_LOCAL3:
+		return "LOCAL3"
+	case LOG_LOCAL4:
+		return "LOCAL4"
+	case LOG_LOCAL5:
+		return "LOCAL5"
+	case LOG_LOCAL6:
+		return "LOCAL6"
+	case LOG_LOCAL7:
+		return "LOCAL7"
+	default:
+		return "UNKNOWN"
+	}
+}
+
+// Set sets the level for the flag.Value interface.
+func (l *Priority) Set(s string) error {
+	f, err := FacilityPriority(s)
+	if err != nil {
+		return err
+	}
+	*l = f
+	return nil
+}
+
+// Get gets the level for the flag.Getter interface.
+func (l *Priority) Get() interface{} {
+	return *l
+}
