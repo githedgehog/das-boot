649dc72c295383cd8895d81ad6f07b221a0039a9
Marcus Heese <marcus@githedgehog.com>
2023-03-14 00:32:31 -0700

    turned zap wrapper into tee wrapper

--- pkg/log/zap_wrapper.go
+++ pkg/log/zap_wrapper.go
@@ -6,85 +6,120 @@ import (
 )
 
 type zapWrapperLogger struct {
-	logger        *zap.Logger
-	sugaredLogger *zap.SugaredLogger
+	loggers        []*zap.Logger
+	sugaredLoggers []*zap.SugaredLogger
 }
 
 var _ Interface = &zapWrapperLogger{}
 
-func NewZapWrappedLogger(logger *zap.Logger) Interface {
+func NewZapWrappedLogger(loggers ...*zap.Logger) Interface {
+	var ls []*zap.Logger
+	var sls []*zap.SugaredLogger
+	for _, logger := range loggers {
+		l := logger.WithOptions(zap.AddCallerSkip(1))
+		ls = append(ls, l)
+		sls = append(sls, l.Sugar())
+	}
 	return &zapWrapperLogger{
-		logger:        logger,
-		sugaredLogger: logger.Sugar(),
+		loggers:        ls,
+		sugaredLoggers: sls,
 	}
 }
 
 // DPanic implements Interface
 func (l *zapWrapperLogger) DPanic(msg string, fields ...zapcore.Field) {
-	l.logger.DPanic(msg, fields...)
+	for _, logger := range l.loggers {
+		logger.DPanic(msg, fields...)
+	}
 }
 
 // DPanicf implements Interface
 func (l *zapWrapperLogger) DPanicf(template string, args ...interface{}) {
-	l.sugaredLogger.DPanicf(template, args...)
+	for _, sugaredLogger := range l.sugaredLoggers {
+		sugaredLogger.DPanicf(template, args...)
+	}
 }
 
 // Debug implements Interface
 func (l *zapWrapperLogger) Debug(msg string, fields ...zapcore.Field) {
-	l.logger.Debug(msg, fields...)
+	for _, logger := range l.loggers {
+		logger.Debug(msg, fields...)
+	}
 }
 
 // Debugf implements Interface
 func (l *zapWrapperLogger) Debugf(template string, args ...interface{}) {
-	l.sugaredLogger.Debugf(template, args...)
+	for _, sugaredLogger := range l.sugaredLoggers {
+		sugaredLogger.Debugf(template, args...)
+	}
 }
 
 // Error implements Interface
 func (l *zapWrapperLogger) Error(msg string, fields ...zapcore.Field) {
-	l.logger.Error(msg, fields...)
+	for _, logger := range l.loggers {
+		logger.Error(msg, fields...)
+	}
 }
 
 // Errorf implements Interface
 func (l *zapWrapperLogger) Errorf(template string, args ...interface{}) {
-	l.sugaredLogger.Errorf(template, args...)
+	for _, sugaredLogger := range l.sugaredLoggers {
+		sugaredLogger.Errorf(template, args...)
+	}
 }
 
 // Fatal implements Interface
 func (l *zapWrapperLogger) Fatal(msg string, fields ...zapcore.Field) {
-	l.logger.Fatal(msg, fields...)
+	for _, logger := range l.loggers {
+		logger.Fatal(msg, fields...)
+	}
 }
 
 // Fatalf implements Interface
 func (l *zapWrapperLogger) Fatalf(template string, args ...interface{}) {
-	l.sugaredLogger.Fatalf(template, args...)
+	for _, sugaredLogger := range l.sugaredLoggers {
+		sugaredLogger.Fatalf(template, args...)
+	}
 }
 
 // Info implements Interface
 func (l *zapWrapperLogger) Info(msg string, fields ...zapcore.Field) {
-	l.logger.Info(msg, fields...)
+	for _, logger := range l.loggers {
+		logger.Info(msg, fields...)
+	}
 }
 
 // Infof implements Interface
 func (l *zapWrapperLogger) Infof(template string, args ...interface{}) {
-	l.sugaredLogger.Infof(template, args...)
+	for _, sugaredLogger := range l.sugaredLoggers {
+		sugaredLogger.Infof(template, args...)
+	}
 }
 
 // Panic implements Interface
 func (l *zapWrapperLogger) Panic(msg string, fields ...zapcore.Field) {
-	l.logger.Panic(msg, fields...)
+	for _, logger := range l.loggers {
+		logger.Panic(msg, fields...)
+	}
 }
 
 // Panicf implements Interface
 func (l *zapWrapperLogger) Panicf(template string, args ...interface{}) {
-	l.sugaredLogger.Panicf(template, args...)
+	for _, sugaredLogger := range l.sugaredLoggers {
+		sugaredLogger.Panicf(template, args...)
+	}
 }
 
 // Warn implements Interface
 func (l *zapWrapperLogger) Warn(msg string, fields ...zapcore.Field) {
-	l.logger.Warn(msg, fields...)
+	for _, logger := range l.loggers {
+		logger.Warn(msg, fields...)
+	}
 }
 
 // Warnf implements Interface
 func (l *zapWrapperLogger) Warnf(template string, args ...interface{}) {
-	l.sugaredLogger.Warnf(template, args...)
+	for _, sugaredLogger := range l.sugaredLoggers {
+		sugaredLogger.Warnf(template, args...)
+	}
 }
