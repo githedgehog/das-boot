7546fecc1226b7824c7bc5c6575e65510b4bcac0
Marcus Heese <marcus@githedgehog.com>
2023-03-14 22:01:56 -0700

    progress on stage 0

--- cmd/stage0/main.go
+++ cmd/stage0/main.go
@@ -1,4 +1,98 @@
 package main
 
+import (
+	"fmt"
+	"os"
+
+	"go.githedgehog.com/dasboot/pkg/log/syslog"
+	"go.githedgehog.com/dasboot/pkg/stage"
+	"go.githedgehog.com/dasboot/pkg/stage0"
+	"go.githedgehog.com/dasboot/pkg/stage0/config"
+	"go.githedgehog.com/dasboot/pkg/version"
+
+	"github.com/urfave/cli/v2"
+	"go.uber.org/zap/zapcore"
+)
+
+var (
+	defaultLogLevel = zapcore.InfoLevel
+	defaultFacility = syslog.LOG_LOCAL0
+)
+
 func main() {
+	app := &cli.App{
+		Name:                 "stage0",
+		Usage:                "configures system network and basic operating functionalities",
+		UsageText:            "stage0",
+		Description:          "Should be running in ONIE, and is the first of a series of installer stages withing DAS BOOT",
+		Version:              version.Version,
+		EnableBashCompletion: true,
+		Flags: []cli.Flag{
+			&cli.GenericFlag{
+				Name:  "log-level",
+				Usage: "minimum log level to log at",
+				Value: &defaultLogLevel,
+			},
+			&cli.StringFlag{
+				Name:  "log-format",
+				Usage: "log format to use: json or console (only affects serial console)",
+				Value: "console",
+			},
+			&cli.BoolFlag{
+				Name:  "log-development",
+				Usage: "enables development log settings",
+				Value: false,
+			},
+			&cli.StringFlag{
+				Name:  "syslog-server",
+				Usage: "syslog server IP addresses or hostnames or FQDNs",
+			},
+			&cli.GenericFlag{
+				Name:  "syslog-facility",
+				Usage: "syslog facility to use within syslog messages",
+				Value: &defaultFacility,
+			},
+			&cli.PathFlag{
+				Name:  "config",
+				Usage: "optional configuration file to load which can override settings of the embedded configuration",
+			},
+		},
+		Action: func(ctx *cli.Context) error {
+			return runStage0(ctx)
+		},
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		fmt.Fprintf(os.Stderr, "FATAL: failed to run stage 0: %s\n", err)
+		os.Exit(1)
+	}
+}
+
+func runStage0(ctx *cli.Context) error {
+	// read optional configuration file first
+	configPath := ctx.Path("config")
+	var cfg *config.Stage0
+	if configPath != "" {
+		var err error
+		cfg, err = config.ReadFromFile(configPath)
+		if err != nil {
+			return err
+		}
+	}
+
+	// set up logging
+	// CLI flags
+	var syslogServers []string
+	syslogServer := ctx.String("syslog-server")
+	if syslogServer != "" {
+		syslogServers = append(syslogServers, syslogServer)
+	}
+	logSettings := &stage.LogSettings{
+		Development:    ctx.Bool("log-development"),
+		Level:          *ctx.Generic("log-level").(*zapcore.Level),
+		Format:         ctx.String("log-format"),
+		SyslogServers:  syslogServers,
+		SyslogFacility: *ctx.Generic("syslog-facility").(*syslog.Priority),
+	}
+	return stage0.Run(ctx.Context, cfg, logSettings)
 }
--- pkg/stage0/stage0.go
+++ pkg/stage0/stage0.go
@@ -1,13 +1,32 @@
 package stage0
 
 import (
+	"context"
 	"crypto/x509"
+	"errors"
 	"fmt"
 	"io"
+	"net/http"
 	"os"
 
 	"go.githedgehog.com/dasboot/pkg/config"
+	"go.githedgehog.com/dasboot/pkg/devid"
+	"go.githedgehog.com/dasboot/pkg/dns"
+	"go.githedgehog.com/dasboot/pkg/log"
+	"go.githedgehog.com/dasboot/pkg/net"
+	"go.githedgehog.com/dasboot/pkg/ntp"
+	"go.githedgehog.com/dasboot/pkg/partitions"
+	"go.githedgehog.com/dasboot/pkg/partitions/location"
+	"go.githedgehog.com/dasboot/pkg/seeder/ipam"
+	"go.githedgehog.com/dasboot/pkg/stage"
 	configstage "go.githedgehog.com/dasboot/pkg/stage0/config"
+	"go.uber.org/zap"
+)
+
+var l = log.L()
+
+const (
+	vlanName = "mgmt"
 )
 
 func ReadConfig() (*configstage.Stage0, error) {
@@ -62,7 +81,13 @@ func ReadConfig() (*configstage.Stage0, error) {
 	return &cfg, nil
 }
 
-func Run(override *configstage.Stage0) error {
+func Run(ctx context.Context, override *configstage.Stage0, logSettings *stage.LogSettings) error {
+	// setup logging first
+	// TODO: this essentially should never fail, so should be implemented differently I guess
+	if err := stage.InitializeGlobalLogger(ctx, logSettings); err != nil {
+		return fmt.Errorf("stage0: failed to initialize logger: %w", err)
+	}
+
 	// read the embedded configuration first
 	embedded, err := ReadConfig()
 	if err != nil {
@@ -75,7 +100,100 @@ func Run(override *configstage.Stage0) error {
 		return fmt.Errorf("stage0: config validation: %w", err)
 	}
 
-	// now let's start doing stuff
+	// we need to do partition discovery for finding our location UUID
+	devices := partitions.Discover()
+
+	// retrieve location info
+	locationPartition, err := location.Open(devices.GetHedgehogLocationPartition())
+	if err != nil {
+		l.Warn("No location partition found", zap.Error(err))
+	}
+	var locationInfo *location.Info
+	if locationPartition != nil {
+		var err error
+		locationInfo, err = locationPartition.GetLocation()
+		if err != nil {
+			return fmt.Errorf("stage0: retrieving location info: %w", err)
+		}
+	}
+
+	// retrieve device ID
+	hhdevid := devid.ID()
+	if hhdevid == "" {
+		return fmt.Errorf("stage0: failed to determine device ID")
+	}
+
+	// retrieve network interface list
+	netdevs, err := net.GetInterfaces()
+	if err != nil {
+		return fmt.Errorf("stage0: retrieving network interface list: %w", err)
+	}
+
+	// build HTTP client
+	httpClient, err := stage.SeederHTTPClient(cfg.CA, nil, stage.HTTPClientOptionServerCertificateIgnoreExpiryTime)
+	if err != nil {
+		return fmt.Errorf("stage0: building HTTP client: %w", err)
+	}
+
+	// now issue the IPAM request
+	ipamReq := &ipam.Request{
+		Arch:                  stage.Arch(),
+		DevID:                 hhdevid,
+		LocationUUID:          locationInfo.UUID,
+		LocationUUIDSignature: locationInfo.UUIDSig,
+		Interfaces:            netdevs,
+	}
+	ipamResp, err := ipam.DoRequest(ctx, httpClient, ipamReq, cfg.IPAMURL)
+	if err != nil {
+		return fmt.Errorf("stage0: IPAM request: %w", err)
+	}
+
+	// we can configure DNS just once
+	if err := dns.SetSystemResolvers(ipamResp.DNSServers); err != nil {
+		return fmt.Errorf("stage0: configuring system DNS resolver: %w", err)
+	}
+
+	// for the rest we iterate over all IP addresses that we got back
+	// and essentially retry the rest of stage 0 until it works
+	var success bool
+	for netdev, ipa := range ipamResp.IPAddresses {
+		if err := runWith(ctx, logSettings, httpClient, ipamResp, netdev, ipa); err != nil {
+			l.Error("failed to run stage 0 to completion with network interface and IP address pair", zap.String("netdev", netdev), zap.Strings("ipAddresses", ipa), zap.Error(err))
+			if err := net.DeleteVLANDevice(vlanName); err != nil {
+				l.Warn("failed to delete VLAN device", zap.Error(err))
+			}
+			continue
+		}
+		success = true
+		break
+	}
+	if !success {
+		return fmt.Errorf("stage0: failed to run stage 0 to completion on any network interface and IP addresses pair")
+	}
+	return nil
+}
+
+func runWith(ctx context.Context, logSettings *stage.LogSettings, httpClient *http.Client, ipamResp *ipam.Response, netdev string, ipAddresses []string) error {
+	// first things first: configure network interface
+	ipaddrnets, err := net.StringsToIPNets(ipAddresses)
+	if err != nil {
+		return fmt.Errorf("converting IP addresses to IPNets: %w", err)
+	}
+	if err := net.AddVLANDeviceWithIP(netdev, ipamResp.VLAN, vlanName, ipaddrnets); err != nil {
+		return fmt.Errorf("failed to configure network interface: %w", err)
+	}
+
+	// configure the syslog logger so that we're not blind anymore
+	localLogSettings := *logSettings
+	localLogSettings.SyslogServers = ipamResp.SyslogServers
+	if err := stage.InitializeGlobalLogger(ctx, &localLogSettings); err != nil {
+		l.Warn("failed to reinitialize global logger with new settings", zap.Error(err))
+	}
+
+	// now run NTP - we only fail if NTP fails, not if hardware clock sync fails
+	if err := ntp.SyncClock(ctx, ipamResp.NTPServers); err != nil && !errors.Is(err, ntp.ErrHWClockSync) {
+		return fmt.Errorf("failed to sync clock with NTP: %w", err)
+	}
 
 	return nil
 }
