5f1ad275e3d7958c55b29492cc40dc469e0fbb47
Marcus Heese <marcus@githedgehog.com>
2023-03-24 23:22:24 -0700

    Bringing up ONIE switches as QEMU VMs (#44)
    * initial set of scripts to bring up switches
    
    * successfully bringing up switch1 device

---
+++ scripts/init_switch.sh
@@ -0,0 +1,173 @@
+#!/bin/bash
+set -e
+
+# NOTE: MAC addresses should be sequential, and not miss a number. The first MAC will be used as the MAC base for ONIE.
+#
+# Why is this necessary?
+#
+# By default ONIE is reprogramming MAC addresses on startup based on the MAC base addresses in increments.
+# According to Carl Brune this is mainly for historical reason with PowerPC hardware where NICs did not have an EEPROM for
+# holding their own MAC addresses, and therefore ONIE was programming them.
+# Unfortunately for us, the default behaviour of ONIE is still to reprogram this if I see that right. So let's just deal with it.
+#
+# See this link for details: https://github.com/opencomputeproject/onie/issues/751#issuecomment-390730344
+DEFAULT_NETDEVS=(
+    "devid=eth0 mac=0c:20:12:fe:01:01 local_port=127.0.0.1:21001 dest_port=127.0.0.1:21000"
+)
+
+# we cannot pass bash arrays, so we will have to parse that
+# taking "devid=" as the indicator that this is a new entry
+tmp="$NETDEVS"
+NETDEVS=()
+if [ -n "$tmp" ]; then
+    # count=-1
+    for i in $tmp; do
+        if [[ "$i" = devid=* ]]; then
+            ((count=$count+1))
+        fi
+        if [ -z "$FIRST_MAC" ] ; then
+            if [[ "$i" = mac=* ]]; then
+                eval $i
+                FIRST_MAC="$mac"
+            fi
+        fi
+        NETDEVS[$count]="${NETDEVS[$count]} $i"
+    done
+else
+    NETDEVS=("${DEFAULT_NETDEVS[@]}")
+fi
+
+# now read the base MAC address from the first MAC
+IFS=':' read -ra BASEMACADDR <<< "$FIRST_MAC"
+
+### START SETTINGS #########
+SWITCH_NAME=${1:-switch}
+# NETDEVS - see above
+### END SETTINGS ###########
+
+# path where this script resides
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+# executable dependencies for this script
+UUIDGEN=$(which uuidgen)
+WGET=$(which wget)
+UNXZ=$(which unxz)
+SWTPM_SETUP=$(which swtpm_setup)
+# It's our tool. Get it here: https://github.com/githedgehog/onie-qcow2-eeprom-edit
+ONIE_EEPROM_EDIT=$(which onie-qcow2-eeprom-edit)
+
+# let's make a dev folder where we are going to store images
+echo -n "Making development folder for storing images: "
+mkdir -v -p ${SCRIPT_DIR}/../dev/images
+IMAGE_DIR=$( cd -- "${SCRIPT_DIR}/../dev/images" &> /dev/null && pwd )
+echo ${IMAGE_DIR}
+echo
+
+# download OS and UEFI images, etc.pp.
+# TODO: the links below are from my personal ONIE builds which I Uploaded to Google drive.
+# Once we build HONIE, we should replace these with dedicated public release links.
+echo "Downloading OS and UEFI images..."
+if [ -f ${IMAGE_DIR}/onie-kvm_x86_64.qcow2 ]; then
+    echo "ONIE kvm_x86_64 image already downloaded: ${IMAGE_DIR}/onie-kvm_x86_64.qcow2.xz"
+    echo "Delete this file if you want to download it again. Skipping..."
+else
+    echo "Downloading ONIE kvm_x86_64 image..."
+    # NOTE: google drive links like this cannot be downloaded directly, but need some work. There are answers for this on stackoverflow
+    # If the file gets too large, then one needs to use a different URL.
+    # Source: https://medium.com/@acpanjan/download-google-drive-files-using-wget-3c2c025a8b99
+    #$WGET -O ${IMAGE_DIR}/onie-kvm_x86_64.qcow2.xz https://drive.google.com/file/d/1hHDBYSk_vbPvwt68nb_e9qFzut80Hsg6/view?usp=share_link
+    $WGET -O ${IMAGE_DIR}/onie-kvm_x86_64.qcow2.xz "https://docs.google.com/uc?export=download&id=1hHDBYSk_vbPvwt68nb_e9qFzut80Hsg6"
+    echo
+    echo "Extracting ONIE kvm_x86_64 image now, this may take some time... (unxz ${IMAGE_DIR}/onie-kvm_x86_64.qcow2.xz)"
+    $UNXZ ${IMAGE_DIR}/onie-kvm_x86_64.qcow2.xz
+fi
+if [ -f ${IMAGE_DIR}/onie_efi_code.fd ]; then
+    echo "ONIE EFI code flash drive already downloaded: ${IMAGE_DIR}/onie_efi_code.fd"
+    echo "Delete this file if you want to download it again. Skipping..."
+else
+    echo "Downloading ONIE EFI code flash drive..."
+    $WGET -O ${IMAGE_DIR}/onie_efi_code.fd "https://docs.google.com/uc?export=download&id=1eWs37uWarVhvclv3XmjHfo9Eux8HMa8E"
+fi
+if [ -f ${IMAGE_DIR}/onie_efi_vars.fd ]; then
+    echo "Flatcar ONIE EFI variables flash drive already downloaded: ${IMAGE_DIR}/onie_efi_vars.fd"
+    echo "Delete this file if you want to download it again. Skipping..."
+else
+    echo "Downloading ONIE EFI variables flash drive..."
+    $WGET -O ${IMAGE_DIR}/onie_efi_vars.fd "https://docs.google.com/uc?export=download&id=1Jc7Twu5JY7RIkOCl5hbxrj9AakotAC5c"
+fi
+echo
+
+# let's make a dev folder where we generate everything for the switch
+echo -n "Making development folder for switch $SWITCH_NAME: "
+mkdir -v -p ${SCRIPT_DIR}/../dev/$SWITCH_NAME
+DEV_DIR=$( cd -- "${SCRIPT_DIR}/../dev/$SWITCH_NAME" &> /dev/null && pwd )
+echo ${DEV_DIR}
+echo
+
+# copy downloaded images to destination
+echo "Copying images to development directory for switch $SWITCH_NAME..."
+cp -v -f ${IMAGE_DIR}/onie-kvm_x86_64.qcow2 ${DEV_DIR}/os.img
+cp -v -f ${IMAGE_DIR}/onie_efi_code.fd ${DEV_DIR}/efi_code.fd
+cp -v -f ${IMAGE_DIR}/onie_efi_vars.fd ${DEV_DIR}/efi_vars.fd
+echo
+
+# generate UUID
+echo "Generating UUID for virtual machine..."
+$UUIDGEN > ${DEV_DIR}/uuid
+echo
+
+# write network devices to disk which will be consumed by every run command
+echo "Writing network devices to disk at ${DEV_DIR}/netdevs.txt"
+for i in "${NETDEVS[@]}"; do
+    echo "$i" >> ${DEV_DIR}/netdevs.txt
+done
+
+# edit the ONIE EEPROM
+# For example, we will just reuse the just generated UUID
+# because ... why not?!
+cat << EOF > ${DEV_DIR}/onie-eeprom.yaml
+tlvs:
+  product_name: Hedgehog ONIE kvm_x86_64 Virtual Machine
+  part_number: hh-onie-kvm_x86_64-vm-1
+  serial_number: $(< ${DEV_DIR}/uuid)
+  mac_base:
+$(for i in "${BASEMACADDR[@]}"; do
+  echo "  - 0x$i"
+done)
+  manufacture_date: $(date +"%m/%d/%Y %H:%M:%S")
+  device_version: 1
+  label_revision: null
+  platform_name: x86_64-kvm_x86_64-r0
+  onie_version: master-01091853-dirty
+  num_macs: ${#NETDEVS[@]}
+  manufacturer: Caprica Systems
+  country_code: US
+  vendor: Hedgehog
+  diag_version: null
+  service_tag: null
+  vendor_extension: null
+EOF
+echo "ONIE EEPROM: Initializing ONIE EEPROM now from values at ${DEV_DIR}/onie-eeprom.yaml"
+$ONIE_EEPROM_EDIT --log-level=debug write --force --input ${DEV_DIR}/onie-eeprom.yaml ${DEV_DIR}/os.img
+echo
+
+# initialize software TPM
+echo "Initializing software TPM 2.0 now..."
+$SWTPM_SETUP --create-config-files skip-if-exist
+if ! [ -f ${HOME}/.config/swtpm_setup.conf ]; then
+    echo "ERROR: swtpm config files expected at: ${HOME}/.config/swtpm_setup.conf" 1>&2
+    exit 1
+fi
+mkdir -p $DEV_DIR/tpm
+$SWTPM_SETUP \
+  --tpm2 \
+  --tpmstate $DEV_DIR/tpm \
+  --createek \
+  --decryption \
+  --create-ek-cert \
+  --create-platform-cert \
+  --create-spk \
+  --vmid "$SWITCH_NAME" \
+  --overwrite \
+  --display
+echo
---
+++ scripts/run_switch.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+set -e
+
+### START SETTINGS #########
+SWITCH_NAME=${1:-switch}
+VM_NCPUS=${VM_NCPUS:-1}
+VM_MEMORY=${VM_MEMORY:-4096}
+# NETDEVS - being initialized at init time, and encoded into ONIE EEPROM
+### END SETTINGS ###########
+
+QEMU_SYSTEM_X86_64=$(which qemu-system-x86_64)
+TPM2=$(which tpm2)
+
+# path where this script resides
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+if ! [ -d  "${SCRIPT_DIR}/../dev/$SWITCH_NAME" ]; then
+    echo "ERROR: Run ${SCRIPT_DIR}/init_switch.sh $SWITCH_NAME first to initialize switch" 1>&2
+    exit 1
+fi
+DEV_DIR=$( cd -- "${SCRIPT_DIR}/../dev/$SWITCH_NAME" &> /dev/null && pwd )
+echo "Development directory for switch $SWITCH_NAME: ${DEV_DIR}"
+echo
+
+echo "Configured QEMU network devices:"
+qemu_devices=""
+while read -r netdev; do
+    eval $netdev
+    echo "- name: $devid, MAC: $mac, Connection: $local_port -> $dest_port"
+    qemu_devices="${qemu_devices} -netdev socket,id=$devid,udp=$dest_port,localaddr=$local_port -device virtio-net-pci,netdev=$devid,mac=$mac"
+done < ${DEV_DIR}/netdevs.txt
+
+# QEMU VM Settings
+VM_NAME="$SWITCH_NAME"
+VM_UUID=$(<${DEV_DIR}/uuid)
+
+# ensure the local docker registry is running
+$SCRIPT_DIR/run_registry.sh
+
+# run the TPM in the background if it is not already running
+if ! [ -f $DEV_DIR/tpm.pid ]; then
+    function on_exit() {
+        if [ -f $DEV_DIR/tpm.pid ]; then
+            kill $(< $DEV_DIR/tpm.pid) &>/dev/null || true
+        fi
+    }
+    $SCRIPT_DIR/run_switch_tpm.sh $SWITCH_NAME &
+    trap on_exit EXIT
+    sleep 1
+    if ! [ -S $DEV_DIR/tpm.sock.ctrl ]; then
+        echo "ERROR: software TPM failed to start control channel $DEV_DIR/tpm.sock.ctrl" 1>&2
+        exit 1
+    fi
+fi
+
+# This is an ugly workaround in a bug in swtpm:
+# If you specify both --server and --ctrl flags for the socket swtpm,
+# then it exits if you start with QEMU directly. If you run a command,
+# then it will continue to work.
+TPM2TOOLS_TCTI="swtpm:path=$DEV_DIR/tpm.sock" $TPM2 startup
+
+echo
+echo "Running switch $SWITCH_NAME VM now..."
+echo
+echo "You can access this VM through several mechanisms:"
+echo "1. Serial port:"
+echo "     make access-switch-$SWITCH_NAME-serial"
+echo "     socat -,rawer,escape=0x1d unix-connect:$DEV_DIR/serial.sock"
+echo "     nc -U $DEV_DIR/serial.sock"
+echo
+echo "2. QEMU monitor"
+echo "     make access-switch-$SWITCH_NAME-monitor"
+echo "     nc -U $DEV_DIR/monitor.sock"
+echo
+echo "3. QEMU QNP: $DEV_DIR/qnp.sock"
+echo "     nc -U $DEV_DIR/qnp.sock"
+echo
+echo "4. ONIE: in the ONIE rescue shell (or any ONIE shell) as well as ONIE GRUB, use the following login:"
+echo "     Username: root"
+echo "     Password: onie"
+echo
+
+$QEMU_SYSTEM_X86_64 \
+  -name "$VM_NAME" \
+  -uuid "$VM_UUID" \
+  -m "$VM_MEMORY" \
+  -machine q35,accel=kvm,smm=on -cpu host -smp "$VM_NCPUS" \
+  $qemu_devices \
+  -object rng-random,filename=/dev/urandom,id=rng0 -device virtio-rng-pci,rng=rng0 \
+  -chardev socket,id=chrtpm,path="$DEV_DIR/tpm.sock.ctrl" -tpmdev emulator,id=tpm0,chardev=chrtpm -device tpm-tis,tpmdev=tpm0 \
+  -drive if=virtio,file="$DEV_DIR/os.img" \
+  -drive if=pflash,file="$DEV_DIR/efi_code.fd",format=raw,readonly=on \
+  -drive if=pflash,file="$DEV_DIR/efi_vars.fd",format=raw \
+  -display none \
+  -vga none \
+  -serial unix:$DEV_DIR/serial.sock,server,nowait \
+  -monitor unix:$DEV_DIR/monitor.sock,server,nowait \
+  -qmp unix:$DEV_DIR/qmp.sock,server,nowait \
+  -global ICH9-LPC.disable_s3=1
